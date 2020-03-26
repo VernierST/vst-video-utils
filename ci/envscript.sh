1ab4343766e4ed46a0422239c7336eca703e34f7
nro2 <robinsonn1@live.com>
2020-03-26 14:49:36 -0700

    Add scripts and package-lock.json and package.json.

---
+++ ci/envscript.sh
@@ -0,0 +1,23 @@
+echo "Cloning EMSDK"
+
+git clone https://github.com/emscripten-core/emsdk.git/
+cd emsdk
+pwd
+
+# Download and install the right SDK tool
+./emsdk install 1.39.7-fastcomp
+
+# Activate the right version of the SDK
+#./emsdk activate sdk-fastcomp-tag-1.38.30-64bit
+./emsdk activate 1.39.7-fastcomp
+
+# activate PATH and other env variables in the current terminal
+source ./emsdk_env.sh
+
+./emsdk list
+
+emcc -v
+
+cd ..
+
+echo $EMSDK
---
+++ ci/installDependencies.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install cmake
+sudo apt-get install git
+sudo apt-get install pkg-config
+sudo apt-get install python
