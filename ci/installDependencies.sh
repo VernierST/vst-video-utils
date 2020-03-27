0bf9ce921e70327b61f82ec1ebf10e2b200c5951
nro2 <robinsonn1@live.com>
2020-03-27 10:45:39 -0700

    Edit python3.7 commands.

--- ci/installDependencies.sh
+++ ci/installDependencies.sh
@@ -1,6 +1,5 @@
 sudo apt-get update
 sudo apt-get install git
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
+sudo add-apt-repository --yes ppa:deadsnakes/ppa
+sudo apt-get update
 sudo apt-get install python3.7
