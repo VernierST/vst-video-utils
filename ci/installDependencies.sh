29b457808da978ee4a78321ce24df56c1287bc2c
nro2 <robinsonn1@live.com>
2020-03-27 10:37:00 -0700

    Add support for python3.7 installation.

--- ci/installDependencies.sh
+++ ci/installDependencies.sh
@@ -1,3 +1,6 @@
 sudo apt-get update
 sudo apt-get install git
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
 sudo apt-get install python3.7
