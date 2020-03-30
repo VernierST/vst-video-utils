deefccaab3149fa926635d101f24c30a518365d3
nro2 <robinsonn1@live.com>
2020-03-30 11:20:20 -0700

    Test publish script.

--- ci/publish.sh
+++ ci/publish.sh
@@ -4,20 +4,20 @@ echo "Pushing changes to GitHub"
 
 
 echo "Deleting previous tags"
-git tag -l | xargs -n 1 git push --delete origin
-git tag | xargs git tag -d
+#git tag -l | xargs -n 1 git push --delete origin
+#git tag | xargs git tag -d
 
 npm install
 
-git checkout -b circle-publish-branch
-git push --set-upstream origin circle-publish-branch
+#git checkout -b circle-publish-branch
+#git push --set-upstream origin circle-publish-branch
 
 echo "Publishing" $1 
 npm run release -- $1 --ci --no-git.requireCleanWorkingDir
 
-git add .
+#git add .
 
-git commit -m Committing $1 changes to repo
-git push
+#git commit -m Committing $1 changes to repo
+#git push
 
 echo "Branch 'circle-publish-branch' created in the GitHub repo.  Please merge with master"
