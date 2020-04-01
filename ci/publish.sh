2a1908aa9d4ba368d6175ece8f78b67757721819
nro2 <robinsonn1@live.com>
2020-04-01 10:57:03 -0700

    Add git pull to publish.

--- ci/publish.sh
+++ ci/publish.sh
@@ -16,5 +16,7 @@ export NVM_DIR="/opt/circleci/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 10.0.0
 
+git pull 
+
 echo "Publishing" $1 
 npm run release -- $1 --ci --no-git.requireCleanWorkingDir
