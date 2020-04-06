9e3d34b0cf00cbdc71c494603b758ed2063d1c56
nro2 <robinsonn1@live.com>
2020-04-06 11:41:13 -0700

    Add new version checking and publish scripts.

---
+++ ci/ciScripts/checkVersions.sh
@@ -0,0 +1,15 @@
+NPMVERSION="$(npm view @vernier/vst-video-utils-build | grep "version:" | awk '{print $2}' | cut -d\' -f2)"
+PACKAGEVERSION="$(grep "version" package.json | awk '{print $2}' | cut -d\" -f2)"
+
+echo $NPMVERSION is latest version on NPM
+echo $PACKAGEVERSION is version number in package.json
+
+if [ "$NPMVERSION" = "$PACKAGEVERSION" ]; then
+    PUBLISH=false
+else
+    PUBLISH=true
+fi
+
+if [ "$PUBLISH" == true]; then
+   sh publishPackage.sh
+fi
