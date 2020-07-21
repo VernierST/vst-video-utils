28891d86a61fdca954c4a96dcd3c614972b71df1
Jennifer Minor <jminor@vernier.com>
2020-07-20 17:12:40 -0700

    why not

--- ci/ciScripts/installDependencies.sh
+++ ci/ciScripts/installDependencies.sh
@@ -2,5 +2,5 @@ sudo apt-get update
 sudo apt-get install git
 sudo add-apt-repository --yes ppa:deadsnakes/ppa
 sudo apt-get update
-#sudo apt-get install python3.7
+sudo apt-get install python3.7
 #pyenv global 3.7.0
